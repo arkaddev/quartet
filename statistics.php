d391b15593c2b8743c9b4fe82932b4bec41db662
arkaddev <arek1994mr@gmail.com>
2024-02-21 14:18:59 +0100

    added new chart.php

--- statistics.php
+++ statistics.php
@@ -99,7 +99,6 @@ $username = $_SESSION['username'];
             // Licznik miejsca na liście
             $miejsce = 1;
           ?>
-      
        <canvas id="myChart"></canvas>
 
     <script>
@@ -108,6 +107,12 @@ $username = $_SESSION['username'];
         var labels = Object.keys(usersData);
         var data = Object.values(usersData);
 
+        // Konwertuj sumę czasu z minut na godziny
+        for (var i = 0; i < data.length; i++) {
+            var hours = data[i] / 60;
+            data[i] = hours.toFixed(2); // Zaokrąglenie do dwóch miejsc po przecinku
+        }
+
         // Konfiguracja wykresu
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
@@ -115,7 +120,7 @@ $username = $_SESSION['username'];
             data: {
                 labels: labels,
                 datasets: [{
-                    label: 'Suma czasu (minuty)',
+                    label: 'Suma czasu (godziny)',
                     data: data,
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     borderColor: 'rgba(75, 192, 192, 1)',
@@ -133,6 +138,7 @@ $username = $_SESSION['username'];
             }
         });
     </script>
+    
       
       <?php
       
@@ -146,8 +152,117 @@ $username = $_SESSION['username'];
         ?>
       
   
+      <h3>Wspólny dzienny czas ćwiczeń</h3>
+
+      
+      <?php
+// Otwieramy plik danych
+$file = fopen("admin/data.txt", "r");
+
+// Inicjujemy tablicę, która będzie przechowywać sumę liczb oraz datę dla każdego dnia
+$sums_by_day = array();
+
+// Odczytujemy plik linia po linii
+while (!feof($file)) {
+    $line = fgets($file); // Odczytujemy jedną linię z pliku
+    $data = explode(",", $line); // Rozdzielamy dane po przecinkach
+
+    // Sprawdzamy, czy linia zawiera wszystkie elementy: imię, liczba oraz data
+    if (count($data) == 3) {
+        $name = trim($data[0]);
+        $number = intval(trim($data[1]));
+        $date = trim($data[2]);
+
+        // Rozdzielamy datę na części: dzień, miesiąc, rok
+        $date_parts = explode(" ", $date);
+        $day = intval($date_parts[0]);
+
+        // Tworzymy pełną datę
+        $full_date = implode(" ", array_slice($date_parts, 0, 3));
+
+        // Dodajemy liczbę i datę do sumy dla danego dnia
+        if (isset($sums_by_day[$day])) {
+            $sums_by_day[$day]['sum'] += $number;
+            $sums_by_day[$day]['date'] = $full_date;
+        } else {
+            $sums_by_day[$day] = array('sum' => $number, 'date' => $full_date);
+        }
+    }
+}
+
+// Zamykamy plik
+fclose($file);
+
+    ?>  
+      
+      
+            <canvas id="myChart2" width="800" height="400"></canvas>
+
+<script>
+    // Dane sumaryczne
+    var sumsByDay = <?php echo json_encode($sums_by_day); ?>;
+
+    // Przygotowanie danych do wykresu
+    var labels = [];
+    var data = [];
+    for (var day in sumsByDay) {
+        if (sumsByDay.hasOwnProperty(day)) {
+            labels.push("Dzień " + day);
+            data.push(sumsByDay[day]['sum']);
+        }
+    }
+  
+  
+  
+  
+        // Konwertuj sumę czasu z minut na godziny
+        for (var i = 0; i < data.length; i++) {
+            var hours = data[i] / 60;
+            data[i] = hours.toFixed(2); // Zaokrąglenie do dwóch miejsc po przecinku
+        }
+  
+  
+  
+  
+
+    // Utworzenie wykresu liniowego
+    var ctx = document.getElementById('myChart2').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Wspólny dzienny czas ćwiczeń',
+                data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+      
+      
+    <?php  
+// Wyświetlamy sumę liczb i datę dla każdego dnia, konwertując sumę minut na godziny i minuty
+foreach ($sums_by_day as $day => $data) {
+    $total_minutes = $data['sum'];
+    $hours = floor($total_minutes / 60);
+    $minutes = $total_minutes % 60;
+    echo "Dzień $day ($data[date]): Suma czasu = $hours godzin $minutes minut <br>";
+}
+?>
+      
       
 
+      
       
 </body>
 </html>
