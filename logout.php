15677a3fca53dd7f0e4708a59359bd6d2e703abe
arkaddev <arek1994mr@gmail.com>
2024-02-22 07:44:33 +0100

    added user.php and logout.php

---
+++ logout.php
@@ -0,0 +1,63 @@
+<?php
+// Ustawienie czasu ważności sesji na 7 dni
+$lifetime = 604800; // 7 dni w sekundach
+session_set_cookie_params($lifetime);
+session_start();
+
+// Sprawdź, czy użytkownik jest zalogowany
+if(!isset($_SESSION['zalogowany']) || $_SESSION['zalogowany'] !== true) {
+    // Jeśli użytkownik nie jest zalogowany, przekieruj go do strony logowania
+    header("Location: index.html");
+    exit();
+}
+
+// Odczytaj nazwę użytkownika z sesji
+$username = $_SESSION['username'];
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin panel</title>
+    <link rel="stylesheet" href="admin/styleadmin.css">
+   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <header>
+        <h1>Panel użytkownika</h1>
+    </header>
+     <nav>
+       <a href="admin.php" class="image-link"><img src="admin/home.png" alt=""></a>
+       <a href="metronome.php" class="image-link"><img src="admin/metronome.png" alt=""></a>
+       <a href="tuner.php" class="image-link"><img src="admin/tuner.png" alt=""></a>
+       <a href="addexercise.php" class="image-link"><img src="admin/add.png" alt=""></a>
+       <a href="statistics.php" class="image-link"><img src="admin/chart.png" alt=""></a>
+       <a href="user.php" class="image-link"><img src="admin/user.png" alt=""></a>
+    </nav>
+    <div class="container">
+       
+        
+ <?php
+// Wyloguj użytkownika poprzez zniszczenie sesji
+session_start();
+session_destroy();
+
+// Przekieruj użytkownika na stronę logowania
+header("Location: login.php");
+exit;
+?>
+
+      
+      
+      
+      
+      
+  </div>
+    <footer>
+        &copy; 2024 The Broken Cello Quartet
+    </footer>
+  
+</body>
+</html>
