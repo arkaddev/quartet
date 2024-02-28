bf49e6c07a203cc9feafb2fb79a41c442373fbbc
arkaddev <arek1994mr@gmail.com>
2024-02-28 14:08:18 +0100

    session.php was created

--- home.php
+++ home.php
@@ -1,18 +1,4 @@
-<?php
-ini_set('session.gc_maxlifetime', 86400);
-session_start();
-
-//session_set_cookie_params(86400);
-// Sprawdź, czy użytkownik jest zalogowany
-if(!isset($_SESSION['zalogowany']) || $_SESSION['zalogowany'] !== true) {
-    // Jeśli użytkownik nie jest zalogowany, przekieruj go do strony logowania
-    header("Location: login.php");
-    exit();
-}
-
-// Odczytaj nazwę użytkownika z sesji
-$username = $_SESSION['username'];
-?>
+<?php include 'php/session.php'; ?>
 
 <!DOCTYPE html>
 <html lang="pl">
