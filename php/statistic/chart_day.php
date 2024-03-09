2082588a6774ca9863e7a10ef1f3b21c6c2cb57d
arkaddev <arek1994mr@gmail.com>
2024-03-09 23:55:59 +0100

    added chart_day.php

--- php/statistic/chart_day.php
+++ php/statistic/chart_day.php
@@ -1,4 +1,6 @@
 <?php
+// Statystyki na obecny dzien z wykresem
+// Na podstawie chart_general.php
 
 $sumy_uzytkownikow = array(); // Inicjalizacja pustej asocjacyjnej tablicy dla przechowywania sum czasu dla każdego użytkownika
 
@@ -9,7 +11,16 @@ foreach ($lines as $line) { // Przejdź przez każdą linię w tablicy
 
   $parts = explode(',', $line); // Podziel linię na części za pomocą przecinka
   
-  if(count($parts) == 3) { // Jeśli liczba części wynosi 3 (czyli mamy wszystkie oczekiwane dane)
+  
+              
+  $dzien = date('d-m-Y'); // Pobierz miesiąc z daty
+              
+  $datetime = $parts[2]; // $parts[2] zawiera datę i czas
+  $timestamp = strtotime($datetime); // Konwertuj datę i czas na znacznik czasu
+  $dzien_miesiac_i_rok = date("d-m-Y", $timestamp); // Formatuj miesiąc i rok
+  
+  
+  if(count($parts) == 3&& $dzien_miesiac_i_rok==$dzien ) { // Jeśli liczba części wynosi 3 (czyli mamy wszystkie oczekiwane dane)
     $imie = $parts[0];
     $czas_w_minutach = (int)$parts[1]; // Konwersja na liczbę całkowitą
                     
@@ -29,8 +40,7 @@ arsort($sumy_uzytkownikow); // Posortuj tablicę sumy_uzytkownikow według warto
 $miejsce = 1; // Licznik miejsca na liście
 ?>
 
-<canvas id="chartGeneral"></canvas>
-
+<canvas id="chartDay"></canvas>
 
 <?php
 /*
@@ -46,6 +56,8 @@ foreach ($sumy_uzytkownikow as $imie => $suma) {
 ?>
 
 <script>
+      
+
 
 
         // Dane użytkowników i ich sumy czasu
@@ -54,27 +66,26 @@ foreach ($sumy_uzytkownikow as $imie => $suma) {
         var data = Object.values(usersData);
 
         // Konwertuj sumę czasu z minut na godziny
-        for (var i = 0; i < data.length; i++) {
-            var hours = data[i] / 60;
-            data[i] = hours.toFixed(2); // Zaokrąglenie do dwóch miejsc po przecinku
-        }
+        //for (var i = 0; i < data.length; i++) {
+        //    var hours = data[i] / 60;
+        //    data[i] = hours.toFixed(2); // Zaokrąglenie do dwóch miejsc po przecinku
+        //}
 
         // Konfiguracja wykresu
-        var ctx = document.getElementById('chartGeneral').getContext('2d');
+        var ctx = document.getElementById('chartDay').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
-          //type: 'pie',
             data: {
                 labels: labels,
                 datasets: [{
-                    label: 'Suma czasu (godziny)',
+                    label: 'Bieżący dzień',
                     data: data,
                     //backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     backgroundColor: [
       'rgb(255, 99, 132, 0.2)',
       'rgb(54, 162, 235, 0.2)',
       'rgb(128, 256, 128, 0.2)',
-      'rgb(255, 205, 86, 0.2)'            
+      'rgb(255, 205, 86, 0.2)'                
     ],
                   borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1
--- php/statistic/chart_month.php
+++ php/statistic/chart_month.php
@@ -1,32 +1,28 @@
 <?php
-
 // Statystyki na obecny miesiąc z wykresem
 // Na podstawie chart_general.php
 
-            // Inicjalizacja pustej asocjacyjnej tablicy dla przechowywania sum czasu dla każdego użytkownika
-            $sumy_uzytkownikow = array();
+$sumy_uzytkownikow = array(); // Inicjalizacja pustej asocjacyjnej tablicy dla przechowywania sum czasu dla każdego użytkownika
+
+$lines = file("data/data.txt", FILE_IGNORE_NEW_LINES); // Wczytaj wszystkie linie z pliku tekstowego do tablicy $lines
             
-            // Wczytaj wszystkie linie z pliku tekstowego do tablicy $lines
-            $lines = file("data/data.txt", FILE_IGNORE_NEW_LINES);
-            
-            // Przejdź przez każdą linię w tablicy
-            foreach ($lines as $line) {
-                // Podziel linię na części za pomocą przecinka
-                $parts = explode(',', $line);
-                
-            
-              // Pobierz miesiąc z daty
-                $miesiac = date('m-Y');
+
+foreach ($lines as $line) { // Przejdź przez każdą linię w tablicy
+
+  $parts = explode(',', $line); // Podziel linię na części za pomocą przecinka
+  
+  
               
-              $datetime = $parts[2]; // Załóżmy, że $parts[2] zawiera datę i czas
-              $timestamp = strtotime($datetime); // Konwertuj datę i czas na znacznik czasu
-              $miesiac_i_rok = date("m-Y", $timestamp); // Formatuj miesiąc i rok
+  $miesiac = date('m-Y'); // Pobierz miesiąc z daty
               
-              // Jeśli liczba części wynosi 3 (czyli mamy wszystkie oczekiwane dane)
-                if(count($parts) == 3 && $miesiac_i_rok==$miesiac) {
-                    // Przypisz dane do zmiennych
-                    $imie = $parts[0];
-                    $czas_w_minutach = (int)$parts[1]; // Konwersja na liczbę całkowitą
+  $datetime = $parts[2]; // $parts[2] zawiera datę i czas
+  $timestamp = strtotime($datetime); // Konwertuj datę i czas na znacznik czasu
+  $miesiac_i_rok = date("m-Y", $timestamp); // Formatuj miesiąc i rok
+  
+  
+  if(count($parts) == 3&& $miesiac_i_rok==$miesiac ) { // Jeśli liczba części wynosi 3 (czyli mamy wszystkie oczekiwane dane)
+    $imie = $parts[0];
+    $czas_w_minutach = (int)$parts[1]; // Konwersja na liczbę całkowitą
                     
                     // Jeśli użytkownik jest już w tablicy sum, dodaj do niego aktualny czas
                     // W przeciwnym razie, dodaj nowy klucz z aktualnym czasem
@@ -38,19 +34,32 @@
                 }
             }
             
-            // Posortuj tablicę sumy_uzytkownikow według wartości czasu (malejąco)
-            arsort($sumy_uzytkownikow);
             
-            // Licznik miejsca na liście
-            $miejsce = 1;
-          ?>
-       
-      <canvas id="myChart2"></canvas>
+arsort($sumy_uzytkownikow); // Posortuj tablicę sumy_uzytkownikow według wartości czasu (malejąco)
 
-    <script>
+$miejsce = 1; // Licznik miejsca na liście
+?>
+
+<canvas id="chartMonth"></canvas>
+
+<?php
+/*
+// Wyświetl sumy czasu dla każdego użytkownika w kolejności malejącej
+            
+foreach ($sumy_uzytkownikow as $imie => $suma) {
+                $godziny = floor($suma / 60);
+                $minuty = $suma % 60;
+                echo "$miejsce. $imie - Suma czasu: $godziny godzin $minuty minut<br>";
+                $miejsce++;
+            }
+*/
+?>
+
+<script>
       
 
-      
+
+
         // Dane użytkowników i ich sumy czasu
         var usersData = <?php echo json_encode($sumy_uzytkownikow); ?>;
         var labels = Object.keys(usersData);
@@ -63,7 +72,7 @@
         }
 
         // Konfiguracja wykresu
-        var ctx = document.getElementById('myChart2').getContext('2d');
+        var ctx = document.getElementById('chartMonth').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
@@ -71,8 +80,14 @@
                 datasets: [{
                     label: 'Suma czasu (godziny)',
                     data: data,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
+                    //backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    backgroundColor: [
+      'rgb(255, 99, 132, 0.2)',
+      'rgb(54, 162, 235, 0.2)',
+      'rgb(128, 256, 128, 0.2)',
+      'rgb(255, 205, 86, 0.2)'              
+    ],
+                  borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1
                 }]
             },
@@ -86,16 +101,6 @@
                 }
             }
         });
-    </script>
-    
-      
-      <?php
-      
-            // Wyświetl sumy czasu dla każdego użytkownika w kolejności malejącej
-            foreach ($sumy_uzytkownikow as $imie => $suma) {
-                $godziny = floor($suma / 60);
-                $minuty = $suma % 60;
-                echo "$miejsce. $imie - Suma czasu: $godziny godzin $minuty minut<br>";
-                $miejsce++;
-            }
-        ?>
+
+
+</script>
